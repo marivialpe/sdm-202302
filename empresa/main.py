cac70c44f117855529bae6a03eccd12d9c270302
Mariana Vilela <134511083+marivialpe@users.noreply.github.com>
2023-11-29 15:03:07 -0300

    Update main.py

--- empresa/main.py
+++ empresa/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, abort, Api, Resource, request
 app = Flask(__name__)
 api = Api(app)
 ESCOLA = [{'matricula': 1, 'nome': 'Ana', 'nota': 72.00},
@@ -15,27 +15,29 @@ def aborta_se_o_aluno_nao_existe(matricula):
             encontrei = True
 
     if encontrei == False:
-        abort(404, mensagem="O aluno com matricula = {} não existe".format(matricula)) #404:Not Found
+        abort(404, mensagem="O aluno com matricula = {} não existe".format(matricula))
 
-# Parse dos dados enviados na requisição no formato JSON:
+
 parser = reqparse.RequestParser()
 parser.add_argument('matricula', type=int, help='matricula do aluno')
 parser.add_argument('nome', type=str, help='nome do aluno')
 parser.add_argument('nota', type=float, help='nota do aluno')
 
-# Produto:
-# 1) Apresenta um único produto.
-# 2) Remove um único produto.
-# 3) Atualiza (substitui) um produto.
+
 class Aluno(Resource):
     def get(self, matricula):
         aborta_se_o_aluno_nao_existe(matricula)
         return ESCOLA[int(matricula)]
     
     def delete(self, matricula):
-        aborta_se_o_aluno_nao_existe(matricula)
-        del ESCOLA[int(matricula)]
-        return '', 204, #204: No Content
+        return self._delete_aluno(matricula)
+    
+    def _delete_aluno(self, matricula):
+        for i, aluno in enumerate(ESCOLA):
+            if aluno['matricula'] == int(matricula):
+                del ESCOLA[i]
+                return '', 204
+        abort(404, message=f'O aluno com matrícula {matricula} não foi encontrado')
 
     def put(self, matricula):
         aborta_se_o_aluno_nao_existe(matricula)
@@ -46,27 +48,30 @@ class Aluno(Resource):
                aluno['nome'] = args['nome']
                aluno['nota'] = args['nota']
                break
-        return aluno, 200, #200: OK
+        return aluno, 200, 
+
+def put(self):
+        data = request.get_json()
+        if not data or not isinstance(data, list):
+            return jsonify({'message': 'Envie uma lista válida de alunos'}), 400
+        
+        global ESCOLA
+        ESCOLA = data 
+        return jsonify({'message': 'Registros de alunos atualizados com sucesso'}), 200
 
 
-# ListaProduto:
-# 1) Apresenta a lista de produtos.
-# 2) Insere um novo produto.
+
 class ListaAluno(Resource):
     def get(self):
         return ESCOLA
     
-def post(self):
-    args = parser.parse_args()
-    matricula = -1
+    def post(self):
+        args = parser.parse_args()
+        matricula = max(aluno['matricula'] for aluno in ESCOLA) + 1
+        aluno = {'matricula': matricula, 'nome': args['nome'], 'nota': args['nota']}
+        ESCOLA.append(aluno)
+        return aluno, 201  
 
-    for produto in ESCOLA:
-        if int(aluno['matricula']) > matricula:
-            matricula = int(produto['matricula'])
-    matricula = matricula + 1
-    aluno = {'matricula': matricula, 'nome': args['nome'], 'nota': args['nota']}
-    ESCOLA.append(aluno)
-    return aluno, 201, #201: Created
 
 class AlunoSTR(Resource):
     def get(self, nome):
@@ -75,20 +80,27 @@ class AlunoSTR(Resource):
             if aluno['nome'] == nome:
                 return aluno
             
-##class ListaAluno(Resource):
+
     def post(self, nome):
         aborta_se_o_aluno_nao_existe(1)
         for aluno in ESCOLA:
             if aluno['nome'] == nome:
                 return aluno
-##FAZER EM CASA            
 
-##
-## Roteamento de recursos:
-##
+class AtualizarAlunos(Resource):
+    def put(self):
+        data = request.get_json()
+        if not data or not isinstance(data, list):
+            abort(400, message='Envie uma lista válida de alunos') 
+            global ESCOLA
+            ESCOLA = data  
+            return jsonify({'message': 'Registros de alunos atualizados com sucesso'}), 200
+         
+
 api.add_resource(Aluno, '/aluno/<matricula>')
 api.add_resource(ListaAluno, '/aluno')
 api.add_resource(AlunoSTR, '/aluno-str/<nome>')
+api.add_resource(AtualizarAlunos, '/atualizar-alunos')
 
 if __name__ == '__main__':
     app.run(debug=True)
